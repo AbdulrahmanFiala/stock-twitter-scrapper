351a0816f664b219bad7f735ab1c7484548dcdd0
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-03-15 03:43:16 +0200

    Update app.py
    Turning the function into an infinite loop that runs every x number of time

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import time
 from dotenv import load_dotenv
 from twikit import Client
 from twikit.utils import build_query
@@ -32,62 +33,70 @@ client.login(
 
 
 def stock_scrapper(twitter_accounts_list, stock_ticker, time_interval):
+    while True:
+        # Converting the time interval to a compatible format
+        time_to_subtract = timedelta(
+            days=time_interval[0], hours=time_interval[1], minutes=time_interval[2])
 
-    # Converting the time interval to a compatible format
-    time_to_subtract = timedelta(
-        days=time_interval[0], hours=time_interval[1], minutes=time_interval[2])
+        # Subtract the timedelta from the datetime
+        targeted_datetime = datetime.now(timezone.utc) - time_to_subtract
 
-    # Subtract the timedelta from the datetime
-    targeted_datetime = datetime.now(timezone.utc) - time_to_subtract
+        # Converting the targeted datetime to a compatible formate for the options dictionary "since" value
+        formatted_date = targeted_datetime.strftime("%Y-%m-%d")
 
-    # Converting the targeted datetime to a compatible formate for the options dictionary "since" value
-    formatted_date = targeted_datetime.strftime("%Y-%m-%d")
+        # counter of the tweets mentioning the ticker
+        counter_of_qualified_tweets = 0
 
-    # counter of the tweets mentioning the ticker
-    counter_of_qualified_tweets = 0
+        # Targeted stock ticker
+        text = stock_ticker
 
-    # Targeted stock ticker
-    text = stock_ticker
+        # Iterate over the list of targeted twitter accounts
+        for user_name in twitter_accounts_list:
 
-    # Iterate over the list of targeted twitter accounts
-    for user_name in twitter_accounts_list:
+            options = {
+                # The name of the account to be scraped
+                "from_user": user_name,
+                "exact_phrases": None,
+                "or_keywords": None,
+                "exclude_keywords": None,
+                "hashtags": None,
+                "to_user": None,
+                "mentioned_users": None,
+                "filters": None,
+                "exclude_filters": None,
+                "urls": None,
+                # Setting a cutoff date for optimize scraping and less consumption of reading rates
+                "since": formatted_date,
+                "until": None,
+                "positive": None,
+                "negative": None,
+                "question": None
+            }
+            query = build_query(text, options)
 
-        options = {
-            # The name of the account to be scraped
-            "from_user": user_name,
-            "exact_phrases": None,
-            "or_keywords": None,
-            "exclude_keywords": None,
-            "hashtags": None,
-            "to_user": None,
-            "mentioned_users": None,
-            "filters": None,
-            "exclude_filters": None,
-            "urls": None,
-            # Setting a cutoff date for optimize scraping and less consumption of reading rates
-            "since": formatted_date,
-            "until": None,
-            "positive": None,
-            "negative": None,
-            "question": None
-        }
-        query = build_query(text, options)
+            # Search latest tweets using the query generated
+            tweets = client.search_tweet(query, 'latest')
 
-        # Search latest tweets using the query generated
-        tweets = client.search_tweet(query, 'latest')
+            for tweet in tweets:
+                # Convert the tweet datetime to a compatible datetime
+                tweet_datetime = datetime.strptime(
+                    tweet.created_at, "%a %b %d %H:%M:%S %z %Y")
+                # If the tweet was tweeted in the targeted datetime range
+                if targeted_datetime <= tweet_datetime:
+                    counter_of_qualified_tweets += 1
+                    # printing the tweet id for testing purposes
+                    print(tweet, user_name, tweet.created_at)
 
-        for tweet in tweets:
-            # Convert the tweet datetime to a compatible datetime
-            tweet_datetime = datetime.strptime(
-                tweet.created_at, "%a %b %d %H:%M:%S %z %Y")
-            # If the tweet was tweeted in the targeted datetime range
-            if targeted_datetime <= tweet_datetime:
-                counter_of_qualified_tweets += 1
-                # printing the tweet id for testing purposes
-                print(tweet, user_name, tweet.created_at)
+        print(stock_ticker + " was mentioned " + str(counter_of_qualified_tweets) + " times in the last " +
+              str(time_interval[0]) + " days, " + str(time_interval[1]) + " hours, and " + str(time_interval[2]) + " minutes.")
+        print("######################################################################")
 
-    print(stock_ticker + " was mentioned " + str(counter_of_qualified_tweets) + " times in the last " +
-          str(time_interval[0]) + " days, " + str(time_interval[1]) + " hours, and " + str(time_interval[2]) + " seconds.")
+        # Calculate the time interval in seconds
+        time_interval_in_seconds = (
+            time_interval[0] * 24 * 60 * 60) + (time_interval[1] * 60 * 60) + (time_interval[2] * 60)
+        # Sleep for the time interval
+        time.sleep(time_interval_in_seconds)
 
 
-stock_scrapper(["elonmusk", "SawyerMerritt"], "starship", [0, 6, 0])
+stock_scrapper(["Mr_Derivatives", "warrior_0719",
+               "ChartingProdigy", "allstarcharts", "yuriymatso", "TriggerTrades", "AdamMancini4", "CordovaTrades", "Barchart", "RoyLMattox"], "$spx", [1, 12, 0])
